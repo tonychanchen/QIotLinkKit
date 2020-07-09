f479ef9fb4de074bd14e33afd664a895ce0f29f9
eagleychen(陈鹰) <tonychanchen@gmail.com>
2020-07-09 15:15:05 +0800

    commit message

---
+++ Example/QIotLinkKit/QIOTViewController.h
@@ -0,0 +1,13 @@
+//
+//  QIOTViewController.h
+//  QIotLinkKit
+//
+//  Created by tonychanchen@gmail.com on 06/05/2020.
+//  Copyright (c) 2020 tonychanchen@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface QIOTViewController : UIViewController
+
+@end
---
+++ Example/QIotLinkKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  QIotLinkKit
+//
+//  Created by tonychanchen@gmail.com on 06/05/2020.
+//  Copyright (c) 2020 tonychanchen@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "QIOTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QIOTAppDelegate class]));
+    }
+}
